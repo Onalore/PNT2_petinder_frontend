2f1650e289928584d93cd7ab62bd65c9c0b839d9
Mauropestarino <pestarino35@gmail.com>
2023-11-09 20:31:58 -0300

    view personalInfo

--- petinder-frontend/screens/FormPersonalInfo.js
+++ petinder-frontend/screens/FormPersonalInfo.js
@@ -3,11 +3,74 @@ import { Text, StyleSheet, View } from "react-native";
 export default function FormPersonalInfo() {
   return (
     <View style={styles.container}>
-      <Text>FormPersonalInfo</Text>
+      <Text style={styles.title}>Formulario de Información Personal</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nombre"
+        value={nombre}
+        onChangeText={(text) => setNombre(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Apellido"
+        value={apellido}
+        onChangeText={(text) => setApellido(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Fecha de Nacimiento"
+        value={fechaNacimiento}
+        onChangeText={(text) => setFechaNacimiento(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Dirección"
+        value={direccion}
+        onChangeText={(text) => setDireccion(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Localidad"
+        value={localidad}
+        onChangeText={(text) => setLocalidad(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Provincia"
+        value={provincia}
+        onChangeText={(text) => setProvincia(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Piso (opcional)"
+        value={piso}
+        onChangeText={(text) => setPiso(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Teléfono"
+        value={telefono}
+        onChangeText={(text) => setTelefono(text)}
+        keyboardType="numeric"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Instagram"
+        value={instagram}
+        onChangeText={(text) => setInstagram(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Facebook"
+        value={facebook}
+        onChangeText={(text) => setFacebook(text)}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleFormSubmit}>
+        <Text style={styles.buttonText}>Enviar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -15,4 +78,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    padding: 8,
+    width: '100%',
+  }
 });
