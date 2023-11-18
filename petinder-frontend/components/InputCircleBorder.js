9449dab158a89d4e2ba527857b27601995fdfb86
Ona <olore@atioinc.com>
2023-11-18 20:15:28 -0300

    Componente input circle border

---
+++ petinder-frontend/components/InputCircleBorder.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+import appsettings from "../appsettings.json";
+
+const InputCircleBorder = ({ placeholder, value, onChangeText }) => {
+  return (
+    <TextInput
+      style={styles.input}
+      placeholder={placeholder}
+      value={value}
+      onChangeText={onChangeText}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 120, // Ajusta el radio de los bordes según tu preferencia
+    padding: 10,
+    width: "100%",
+  },
+});
+
+export default InputCircleBorder;
