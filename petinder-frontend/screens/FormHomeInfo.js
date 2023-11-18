90940d2d8e4b1aebad6f55972add4f503c152943
Ona <olore@atioinc.com>
2023-11-18 20:20:49 -0300

    Estilo y reestructuración formHomeInfo

--- petinder-frontend/screens/FormHomeInfo.js
+++ petinder-frontend/screens/FormHomeInfo.js
@@ -1,111 +1,123 @@
-﻿import React, { useState } from 'react';
-import { StyleSheet, ScrollView, Button, Text } from "react-native";
+﻿import React, { useState } from "react";
+import { StyleSheet, ScrollView, Text, Image, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import Card from '../components/Card';
-import {CheckBox} from "react-native-elements"
+import Card from "../components/Card";
+import Title from "../components/Title";
+import Button from "../components/Button";
+import InputBottomLine from "../components/InputBottomLine";
+import CheckboxCircle from "../components/CheckboxCircle";
 
 const FormHomeInfo = () => {
-    const navigation = useNavigation();
-    const [m2Disponibles, setm2Disponibles] = useState("");
-    const [animales, setAnimales] = useState("");
-    const [horasAlDia, setHorasAlDia] = useState("");
-    const [ninios, setNinios] = useState("");
-    const [tamanio, setTamanio] = useState("");
-    
+  const navigation = useNavigation();
+  const [m2Disponibles, setm2Disponibles] = useState("");
+  const [animales, setAnimales] = useState("");
+  const [horasAlDia, setHorasAlDia] = useState("");
+  const [ninios, setNinios] = useState("");
+  const [tamanio, setTamanio] = useState("");
 
-    return (
-        <ScrollView contentContainerStyle={styles.container}>
-            <Card
-                title="¿Cuantos m2 dispone de su hogar?"
-                placeholder="Escribe aquí"
-                value={m2Disponibles}
-                keyboardType="numeric"
-                onChangeText={(text) => setm2Disponibles(text)}
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Image source={require("../assets/logo.png")} style={styles.imagen} />
+      <Title title={"Sobre tu hogar"} />
+      <Card
+        title="¿Cuantos m2 dispone de su hogar?"
+        child={
+          <InputBottomLine
+            placeholder="Tu respuesta"
+            value={m2Disponibles}
+            onChangeText={(text) => setm2Disponibles(text)}
+          />
+        }
+      />
+      <Card
+        title="¿Tiene otros animales?"
+        child={
+          <View>
+            <CheckboxCircle
+              title="Sí"
+              checked={animales}
+              onPress={() => setAnimales(true)}
             />
-
-        
-        <Text>¿Tiene otros animales?</Text>
-
-            <CheckBox
-                checkedIcon="dot-circle-o"
-                uncheckedIcon="circle-o"
-                title="Sí"
-                checked={animales === true}
-                onPress={() => setAnimales(true)}
+            <CheckboxCircle
+              title="No"
+              checked={!animales}
+              onPress={() => setAnimales(false)}
             />
-            <CheckBox
-                checkedIcon="dot-circle-o"
-                uncheckedIcon="circle-o"
-                title="No"
-                checked={animales === false}
-                onPress={() => setAnimales(false)}
+          </View>
+        }
+      />
+      <Card
+        title="¿Convive con niños?"
+        child={
+          <View>
+            <CheckboxCircle
+              title="Sí"
+              checked={ninios}
+              onPress={() => setNinios(true)}
             />
-            
-            <Text>¿Convive con niños?</Text>
-        
-            <CheckBox
-                checkedIcon="dot-circle-o"
-                uncheckedIcon="circle-o"
-                title="Sí"
-                checked={ninios === true}
-                onPress={() => setNinios(true)}
+            <CheckboxCircle
+              title="No"
+              checked={!ninios}
+              onPress={() => setNinios(false)}
             />
-            <CheckBox
-                checkedIcon="dot-circle-o"
-                uncheckedIcon="circle-o"
-                title="No"
-                checked={ninios === false}
-                onPress={() => setNinios(false)}
+          </View>
+        }
+      />
+      <Card
+        title="¿Cuantas horas al dia pasas en tu casa?"
+        child={
+          <InputBottomLine
+            placeholder="Tu respuesta"
+            value={horasAlDia}
+            onChangeText={(text) => setHorasAlDia(text)}
+          />
+        }
+      />
+      <Card
+        title="¿Cuál es el tamaño de mascota ideal para tu hogar?"
+        child={
+          <View>
+            <CheckboxCircle
+              title="Pequeño"
+              checked={tamanio === "Pequenio"}
+              onPress={() => setTamanio("Pequenio")}
             />
-
-            <Card
-                title="¿Cuantas horas al dia pasas en tu casa?"
-                placeholder="Escribe aquí"
-                value={horasAlDia}
-                onChangeText={(text) => setHorasAlDia(text)}
+            <CheckboxCircle
+              title="Mediano"
+              checked={tamanio === "Mediano"}
+              onPress={() => setTamanio("Mediano")}
             />
-
-            <Text>¿Que tamaño de mascota desea?</Text>
-        
-            <CheckBox
-                checkedIcon="dot-circle-o"
-                uncheckedIcon="circle-o"
-                title="Pequeño"
-                checked={tamanio === "Pequenio"}
-                onPress={() => setTamanio("Pequenio")}
+            <CheckboxCircle
+              title="Grande"
+              checked={tamanio === "Grande"}
+              onPress={() => setTamanio("Grande")}
             />
-            
-            <CheckBox
-                checkedIcon="dot-circle-o"
-                uncheckedIcon="circle-o"
-                title="Mediano"
-                checked={tamanio === "Mediano"}
-                onPress={() => setTamanio("Mediano")}
-            />
-
-            <CheckBox
-                checkedIcon="dot-circle-o"
-                uncheckedIcon="circle-o"
-                title="Grande"
-                checked={tamanio === "Grande"}
-                onPress={() => setTamanio("Grande")}
-            />
-
-            <Button
-                title="Continuar"
-                onPress={() => navigation.navigate('FormPetInfo')}
-            />
-        </ScrollView>
-    );
+          </View>
+        }
+      />
+      <Button
+        text={"Continuar"}
+        onPress={() => navigation.navigate("FormPetInfo")}
+      />
+    </ScrollView>
+  );
 };
 
 export default FormHomeInfo;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
+    display: "flex",
+    backgroundColor: "#ffffff",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start",
+    paddingTop: "10%",
+    paddingBottom: "4%",
+    paddingHorizontal: "8%",
+  },
+  imagen: {
+    width: 60,
+    height: 100,
+    marginBottom: "5%",
   },
 });
