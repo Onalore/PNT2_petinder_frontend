93e3b857b32545bdd4b30be90e5ef2b8ea822774
Ona <olore@atioinc.com>
2023-11-18 21:43:37 -0300

    Cambios en FormPetInfo

--- petinder-frontend/screens/FormPetInfo.js
+++ petinder-frontend/screens/FormPetInfo.js
@@ -1,16 +1,10 @@
 import React, { useState } from "react";
-import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  StyleSheet,
-  ScrollView,
-  Image,
-} from "react-native";
+import { StyleSheet, ScrollView, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import Card from "../components/Card";
 import Title from "../components/Title";
+import InputBottomLine from "../components/InputBottomLine";
+import Button from "../components/Button";
 
 export default function FormPetInfo() {
   const navigation = useNavigation();
@@ -41,50 +35,55 @@ export default function FormPetInfo() {
       <Title title={"Sobre la Mascota"} />
       <Card
         title="¿Qué rol crees que debe ocupar el animal en tu hogar?"
-        placeholder="Ej. Compañía, guardián, etc."
-        value={rol}
-        onChangeText={(text) => setRol(text)}
+        child={
+          <InputBottomLine
+            placeholder="Ej. Compañía, guardián, etc."
+            value={rol}
+            onChangeText={(text) => setRol(text)}
+          />
+        }
       />
       <Card
-        title="Describe el carácter deseado del animal que quieres adoptar:"
-        placeholder="Ej. Juguetón, tranquilo, cariñoso, etc."
-        value={caracter}
-        onChangeText={(text) => setCaracter(text)}
+        title="Describe el carácter deseado del animal que quieres adoptar"
+        child={
+          <InputBottomLine
+            placeholder="Ej. Juguetón, tranquilo, cariñoso, etc."
+            value={caracter}
+            onChangeText={(text) => setCaracter(text)}
+          />
+        }
       />
-      <Text style={styles.label}>
-        Describe el carácter deseado del animal que quieres adoptar:
-      </Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Ej. Juguetón, tranquilo, cariñoso, etc."
-        value={caracter}
-        onChangeText={(text) => setCaracter(text)}
-      />
-
       <Card
         title="Ante una inadaptación o problema de comportamiento en el animal que adoptes, ¿qué harías?"
-        placeholder="Ej. Consultar a un entrenador, buscar soluciones, etc."
-        value={inadaptacion}
-        onChangeText={(text) => setInadaptacion(text)}
+        child={
+          <InputBottomLine
+            placeholder="Ej. Consultar a un entrenador, buscar soluciones, etc."
+            value={inadaptacion}
+            onChangeText={(text) => setInadaptacion(text)}
+          />
+        }
       />
-
       <Card
         title="¿En qué circunstancias considerarías devolver al animal adoptado? ¿Lo harías?"
-        placeholder="Ej. Cambio de circunstancias personales, problemas de salud, etc."
-        value={devolucion}
-        onChangeText={(text) => setDevolucion(text)}
+        child={
+          <InputBottomLine
+            placeholder="Ej. Cambio de circunstancias personales, problemas de salud, etc."
+            value={devolucion}
+            onChangeText={(text) => setDevolucion(text)}
+          />
+        }
       />
-
       <Card
         title="¿Cómo educas/castigas a un animal? ¿Qué medidas tomas para hacerlo?"
-        placeholder="Ej. Reforzamiento positivo, tiempo fuera, etc."
-        value={educacion}
-        onChangeText={(text) => setEducacion(text)}
+        child={
+          <InputBottomLine
+            placeholder="Ej. Reforzamiento positivo, tiempo fuera, etc."
+            value={educacion}
+            onChangeText={(text) => setEducacion(text)}
+          />
+        }
       />
-
-      <TouchableOpacity style={styles.button} onPress={handleFormSubmit}>
-        <Text style={styles.buttonText}>Enviar</Text>
-      </TouchableOpacity>
+      <Button text="Enviar" onPress={handleFormSubmit} />
     </ScrollView>
   );
 }
@@ -108,27 +107,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "bold",
     marginBottom: 16,
-  },
-  label: {
-    fontSize: 16,
-    marginBottom: 8,
-  },
-  input: {
-    height: 40,
-    borderColor: "gray",
-    borderWidth: 1,
-    marginBottom: 16,
-    padding: 8,
-    width: "100%",
-  },
-  button: {
-    backgroundColor: "blue",
-    padding: 10,
-    borderRadius: 5,
-    marginTop: 20,
-  },
-  buttonText: {
-    color: "white",
-    textAlign: "center",
   },
 });
