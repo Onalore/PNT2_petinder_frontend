38b2023a2bd255dfb759855269aafa1d31043442
Ona <olore@atioinc.com>
2023-11-23 00:41:41 -0300

    Cambios css

--- petinder-frontend/screens/Home.js
+++ petinder-frontend/screens/Home.js
@@ -1,67 +1,75 @@
-import { Text, StyleSheet, View, Image } from "react-native";
+import { Text, StyleSheet, View, Image, TouchableOpacity } from "react-native";
 import Title from "../components/Title";
-import Button from "../components/Button";
 import appsettings from "../appsettings.json";
 import { useNavigation } from "@react-navigation/native";
 import CardIconText from "../components/CardIconText";
+//import * as GoogleSingIn from "expo-auth-session/provider/google";
 
 export default function Home() {
-    const navigation = useNavigation();
-    return (
-        <View style={styles.container}>
-            <Image source={require("../assets/logo.png")} style={styles.imagen} />
-            <Title tittle={"Valida tu identidad"} />
-            <CardIconText
-                icon={"email"}
-                title={"Email"}
-                text={"Ingresa con tu email"}
-                onPress={() => navigation.navigate("Login")}
-            />
-            <CardIconText
-                icon={"google"}
-                title={"Google"}
-                text={"Ingresa con google"}
-            />
-            <View style={styles.textAlignment}>
-                <Text style={styles.texto1}>Regístrate</Text>
-                <Text style={styles.texto2}>si no tienes cuenta</Text>
-            </View>
-            <Button
-                text={"Continuar"}
-                onPress={() => navigation.navigate("FormHomeInfo")}
-            />
-        </View>
-    );
+  const navigation = useNavigation();
+  //   const { request, response, promptAsync } = Google.useAuthRequest({
+  //     androidClientId:
+  //       "185014890847-p6p59l9kb3l088esl1sjkdbikn1nts48.apps.googleusercontent.com",
+  //   });
+
+  const Login = () => {};
+
+  return (
+    <View style={styles.container}>
+      <Image source={require("../assets/logo.png")} style={styles.imagen} />
+      <Title tittle={"Valida tu identidad"} />
+      <TouchableOpacity>
+        <CardIconText
+          icon={"email"}
+          title={"Email"}
+          text={"Ingresa con tu email"}
+          onPress={() => navigation.navigate("Login")}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <CardIconText
+          icon={"google"}
+          title={"Google"}
+          text={"Ingresa con google"}
+        />
+      </TouchableOpacity>
+      <View style={styles.textAlignment}>
+        <Text style={styles.texto1}>Regístrate</Text>
+        <Text style={styles.texto2}>si no tienes cuenta</Text>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#FFFFFF",
-        alignItems: "center",
-        justifyContent: "flex-start",
-    },
-    imagen: {
-        width: 80,
-        height: 130,
-    },
-    textAlignment: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        paddingHorizontal: 16,
-        alignItems: "center",
-    },
-    texto1: {
-        fontSize: 16,
-        fontWeight: "bold",
-        color: appsettings.colors.primary,
-    },
-    texto2: {
-        fontSize: 16,
-        color: appsettings.colors.primary,
-        marginLeft: 5,
-    },
+  container: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#FFFFFF",
+    height: "100%",
+  },
+  imagen: {
+    width: 80,
+    height: 130,
+    marginBottom: "16%",
+  },
+  textAlignment: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 16,
+    alignItems: "center",
+    marginTop: "1%",
+    marginBottom: "10%",
+  },
+  texto1: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: appsettings.colors.primary,
+  },
+  texto2: {
+    fontSize: 16,
+    color: appsettings.colors.primary,
+    marginLeft: 5,
+  },
 });
